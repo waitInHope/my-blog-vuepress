9f85dbcf83ca5de42675f9c067c8ffad1da2828f
zhangzhendong.zzd <zhangzhendong.zzd@bytedance.com>
2023-01-18 01:35:47 +0800

    feat: 项目实现代码自动同步到Github和Gitee

--- deploy.sh
+++ deploy.sh
@@ -9,11 +9,18 @@ yarn build
 # 进入生成的文件夹
 cd docs/.vuepress/dist
 
+# 同步Github和Gitee的方案一：借助github的action实现仓库代码同步到Gitee
+# 拷贝.github/workflows/syncToGitee.yml到dist目录
+cp -r ../../../.github ./
+
 git init
 git add -A
 git commit -m 'deploy'
 
-# 如果发布到 https://<USERNAME>.github.io/<REPO>
+# 发布到 github 的 gh-pages 分支
 git push -f git@github.com:waitInHope/my-blog-vuepress.git master:gh-pages
+
+# 同步Github和Gitee的方案二：直接把代码推送到 Github 和 Gitee 的 gh-pages 分支
+# git push -f git@gitee.com:waitinhope/my-blog-vuepress.git master:gh-pages
 
 cd -
